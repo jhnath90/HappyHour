475b9e6921aab55a4f7fcbfc34db98380dea2b09
Josh Hnath <joshhnath@gmail.com>
2015-09-08 11:09:03 -0400

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
+
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :users
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
