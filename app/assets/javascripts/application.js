aaf6e3b095ed502dd45ec3e6a4ac85b8aca98b7c
carlos medina <Carlos@CM-RMP.local>
2015-09-12 17:43:16 -0400

    Added hamburger menu funtion for opening and closing once it reaches a screensize < 900px

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,36 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+
+    // media query event handler
+if (matchMedia) {
+    var mq = window.matchMedia("(max-width: 900px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+
+    if (mq.matches) {
+         $('#locator').hide();
+        $('.hamburger').click(function() {
+            $('#locator').fadeToggle('slow', function() {
+                $('hamburger').click()
+            }).css({
+                display: 'flex-inline',
+            });
+        });
+    }
+    else {
+        $('#locator').show("slow").css({
+            display: 'block',
+        });;
+
+    }
+
+}
+});
