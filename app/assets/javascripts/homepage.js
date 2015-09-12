a42e306fa2df8cec3bc9f80fc70e06939e6bb599
carlos medina <Carlos@CM-RMP.local>
2015-09-12 17:45:32 -0400

    Recoded the Google api to get the user geolocation while providing markers showing bars within a radius

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,42 +1,109 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
+var infowindow,
+placemarkers = [];
 
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
+
+function placeSearch(map,request) {
+  var map=map;
+  var service = new google.maps.places.PlacesService(map);
+  service.search(request,
+     function(results,status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          var bounds=new google.maps.LatLngBounds();
+          for (var i = 0; i < results.length; ++i) {
+            bounds.extend(results[i].geometry.location);
+            placemarkers.push(createMarker(results[i].geometry.location,
+               map,
+               'http://labs.google.com/ridefinder/images/mm_20_green.png',
+               results[i].name,
+               false, {
+                  fnc:function() {
+                    infowindow.open();
+                }
+
+            }));
+        }
+        map.fitBounds(bounds);
+    }
+}
+);
+
+}
+
+function createMarker(latlng,map,icon,content,center,action) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: latlng,
+    content:content
+});
+  if(icon){marker.setIcon(icon);}
+
+  if(center) {
+    map.setCenter(latlng);
+}
+
+google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(this.content);
+    infowindow.open(map, this);
+});
+
+if(action) {
+    action.fnc(map,action.args);
+}
+return marker;
+}
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.632372, lng: -98.4412865},
-    zoom: 5
+  var location = new google.maps.LatLng(37.632372, -98.4412865),
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: location,
+    zoom: 5,
+    styles: styleArray   // Google Maps call on the styling array
   });
-  var infoWindow = new google.maps.InfoWindow({map: map});
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+  infowindow = new google.maps.InfoWindow({
+    content: "holding",
+  });
+  navigator.geolocation.getCurrentPosition(function(place) {
+    createMarker(
+      new google.maps.LatLng(place.coords.latitude,
+        place.coords.longitude),
+      map,
+      null,
+      'your current position',
+      true,
+      {
+         fnc:placeSearch,
+         args:{
+           radius: 5000,
+           types: ['bar'],
+           location:new google.maps.LatLng(place.coords.latitude,
+             place.coords.longitude)
+       }
+   }
+   );
+});
+}
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+var styleArray = [{
+  featureType: "all",
+  stylers: [{
+    saturation: -80
+}]},
+{
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [{
+      hue: "#00ffee" },
+      {
+        saturation: 50
+    }]},
+    {
+        featureType: "poi.business",
+        elementType: "labels",
+        stylers: [{
+          visibility: "off"
+      }]
   }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
+  ];
